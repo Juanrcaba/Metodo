f2cd7543fd3ad28484fb435722cc07f2bafcc8f5
Juan Santos <juanrscaba@gmail.com>
2021-04-29 18:56:18 -0400

    Add project files.

---
+++ Metodo/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿
+namespace Metodo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.txt_salario = new System.Windows.Forms.TextBox();
+            this.txt_afp = new System.Windows.Forms.TextBox();
+            this.txt_ars = new System.Windows.Forms.TextBox();
+            this.txt_isr = new System.Windows.Forms.TextBox();
+            this.txt_deduccion = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_sueldoNeto = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.btn_nuevo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_nombre.Location = new System.Drawing.Point(112, 36);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(238, 32);
+            this.txt_nombre.TabIndex = 0;
+            // 
+            // txt_salario
+            // 
+            this.txt_salario.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_salario.Location = new System.Drawing.Point(112, 90);
+            this.txt_salario.Name = "txt_salario";
+            this.txt_salario.Size = new System.Drawing.Size(197, 32);
+            this.txt_salario.TabIndex = 1;
+            // 
+            // txt_afp
+            // 
+            this.txt_afp.Enabled = false;
+            this.txt_afp.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_afp.Location = new System.Drawing.Point(34, 159);
+            this.txt_afp.Name = "txt_afp";
+            this.txt_afp.Size = new System.Drawing.Size(138, 32);
+            this.txt_afp.TabIndex = 2;
+            // 
+            // txt_ars
+            // 
+            this.txt_ars.Enabled = false;
+            this.txt_ars.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_ars.Location = new System.Drawing.Point(247, 159);
+            this.txt_ars.Name = "txt_ars";
+            this.txt_ars.Size = new System.Drawing.Size(138, 32);
+            this.txt_ars.TabIndex = 3;
+            // 
+            // txt_isr
+            // 
+            this.txt_isr.Enabled = false;
+            this.txt_isr.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_isr.Location = new System.Drawing.Point(460, 159);
+            this.txt_isr.Name = "txt_isr";
+            this.txt_isr.Size = new System.Drawing.Size(138, 32);
+            this.txt_isr.TabIndex = 4;
+            // 
+            // txt_deduccion
+            // 
+            this.txt_deduccion.Enabled = false;
+            this.txt_deduccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_deduccion.Location = new System.Drawing.Point(84, 234);
+            this.txt_deduccion.Name = "txt_deduccion";
+            this.txt_deduccion.Size = new System.Drawing.Size(138, 32);
+            this.txt_deduccion.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "NOMBRE: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "SALARIO: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(81, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "AFP";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(304, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "ARS";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(516, 194);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "ISR";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(396, 273);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "SUELDO NETO";
+            // 
+            // txt_sueldoNeto
+            // 
+            this.txt_sueldoNeto.Enabled = false;
+            this.txt_sueldoNeto.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_sueldoNeto.Location = new System.Drawing.Point(381, 238);
+            this.txt_sueldoNeto.Name = "txt_sueldoNeto";
+            this.txt_sueldoNeto.Size = new System.Drawing.Size(138, 32);
+            this.txt_sueldoNeto.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(74, 269);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(157, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "TOTAL DEDUCCIONES";
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_calcular.Location = new System.Drawing.Point(121, 325);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(101, 33);
+            this.btn_calcular.TabIndex = 14;
+            this.btn_calcular.Text = "CALCULAR";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // btn_nuevo
+            // 
+            this.btn_nuevo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_nuevo.Location = new System.Drawing.Point(345, 325);
+            this.btn_nuevo.Name = "btn_nuevo";
+            this.btn_nuevo.Size = new System.Drawing.Size(101, 33);
+            this.btn_nuevo.TabIndex = 15;
+            this.btn_nuevo.Text = "NUEVO";
+            this.btn_nuevo.UseVisualStyleBackColor = true;
+            this.btn_nuevo.Click += new System.EventHandler(this.btn_nuevo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 396);
+            this.Controls.Add(this.btn_nuevo);
+            this.Controls.Add(this.btn_calcular);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txt_sueldoNeto);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_deduccion);
+            this.Controls.Add(this.txt_isr);
+            this.Controls.Add(this.txt_ars);
+            this.Controls.Add(this.txt_afp);
+            this.Controls.Add(this.txt_salario);
+            this.Controls.Add(this.txt_nombre);
+            this.MaximumSize = new System.Drawing.Size(632, 435);
+            this.MinimumSize = new System.Drawing.Size(632, 435);
+            this.Name = "Form1";
+            this.Text = "Calculadora Salarial";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_nombre;
+        private System.Windows.Forms.TextBox txt_salario;
+        private System.Windows.Forms.TextBox txt_afp;
+        private System.Windows.Forms.TextBox txt_ars;
+        private System.Windows.Forms.TextBox txt_isr;
+        private System.Windows.Forms.TextBox txt_deduccion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_sueldoNeto;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.Button btn_nuevo;
+    }
+}
+
---
+++ Metodo/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metodo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double _ARS = 0, _AFP = 0, _ISR = 0, _DEDUCCION = 0, _SUELDONETO = 0, _SALARIO = 0;
+
+        claNomina Calculadora = new claNomina();
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txt_salario.Text,out _SALARIO)) {
+
+                _SALARIO = double.Parse(txt_salario.Text);
+                procesarCalculo();
+                MostrarDatos();
+            }
+            else
+            {
+                MessageBox.Show("Debe insertar valor numerico");
+                txt_salario.Clear();
+                txt_salario.Focus();
+
+            }
+            
+        }
+
+        private void btn_nuevo_Click(object sender, EventArgs e)
+        {
+            Limpiar();            
+        }
+
+        //metodo limpiar componentes
+        void Limpiar()
+        {
+            txt_nombre.Clear();
+            txt_salario.Clear();
+            txt_afp.Clear();
+            txt_ars.Clear();
+            txt_isr.Clear();
+            txt_sueldoNeto.Clear();
+            txt_deduccion.Clear();
+            txt_nombre.Enabled = true;
+            txt_salario.Enabled = true;
+            txt_nombre.Focus();
+        }
+        //metodo ejecutar calculos
+        void procesarCalculo()
+        {
+            _ARS = Calculadora.calculo_ARS(_SALARIO);
+            _AFP = Calculadora.calculo_AFP(_SALARIO);
+            _DEDUCCION = _ARS + _AFP;
+            _ISR = Calculadora.calculo_ISR(_SALARIO, _DEDUCCION);
+
+            if (_ISR != 0) { _DEDUCCION = _ARS + _AFP + _ISR; }
+
+            _SUELDONETO = _SALARIO - _DEDUCCION - _ISR;
+        }
+
+        void MostrarDatos()
+        {
+            txt_nombre.Enabled = false;
+            txt_salario.Enabled = false;
+            txt_salario.Text = _SALARIO.ToString("###,###.##");
+            txt_afp.Text = _AFP.ToString("###,###.##");
+            txt_ars.Text = _ARS.ToString("###,###.##");
+            txt_isr.Text = _ISR.ToString("###,###.##");
+            txt_deduccion.Text = _DEDUCCION.ToString("###,###.##");
+            txt_sueldoNeto.Text = _SUELDONETO.ToString("###,###.##");
+        }
+    }
+    //creacion de la clase nomina
+    class claNomina
+    {
+        //metodos de la clase nomina
+
+       public double calculo_ARS(double salario)
+        {
+            return salario * 0.0304; //descuento ARS es el 3.04% del salario
+        }
+
+        public double calculo_AFP(double salario)
+        {
+            return salario * 0.0287; //descuento AFP es el 2.87% del salario
+        }
+
+       public double calculo_ISR(double salario, double deducciones)
+        {
+            double ISR = 0.00;
+            double _IngresoAnual = (salario - deducciones) * 12;
+            
+            if (_IngresoAnual >= 416220.01 && _IngresoAnual <= 624329)
+            {
+                ISR = ((_IngresoAnual - 416220.01) * 0.15)/12;
+            }
+            else if (_IngresoAnual >= 624329.01 && _IngresoAnual <= 867123)
+            {
+                ISR = (((_IngresoAnual - 624329.01) * 0.20) + 31216) / 12;
+            }
+            else if (_IngresoAnual >= 867123.01)
+            {
+                ISR = (((_IngresoAnual - 624329.01) * 0.20) + 79776) / 12;
+            }
+
+            return ISR;
+
+        }
+    }
+}
